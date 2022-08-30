22933d7f46bf98007fe64b591694471d506a0325
luciussulla <maciek.kowalczyk@yandex.com>
2022-08-30 09:39:54 +0200

    FInish the rate calculation

--- currencyExchange/script.js
+++ currencyExchange/script.js
@@ -6,7 +6,7 @@ const amountEl_two   = document.getElementById('amount-two');
 
 // [currencyEl_one, amountEl_one, currencyEl_two, amountEl_two].forEach(el=> console.log(el))
 
-const rate = document.getElementById('rate'); 
+const rateEl = document.getElementById('rate'); 
 const swap = document.getElementById('swap'); 
 
 // Fetch exchange rates and update the dom 
@@ -20,14 +20,10 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
   .then(res=>res.json())
   .then(data => {
-    console.log(data); 
-    const rate = data.rates[currency_two]; 
-
-    console.log(rate); 
-
-    RTCDataChannelEvent.innerText =  `1 ${currency_one} = ${rate} ${currency_two}`
+    const rate = data.rates[currency_two];
+    rateEl.innerText =  `1 ${currency_one} = ${rate} ${currency_two}`
+    amountEl_two.value = (amountEl_one.value * rate).toFixed(2); 
   })
-
 }
 
 // event listeners
@@ -35,3 +31,13 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate); 
 currencyEl_two.addEventListener('change', calculate); 
 amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', ()=> {
+  const temp = currencyEl_one.value; 
+  currencyEl_one.value = currencyEl_two.value; 
+  currencyEl_two.value = temp; 
+  calculate(); 
+
+})
+
+calculate()
