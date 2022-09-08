d3b3b784c345b354a18893c28bacccf8c519bd5b
luciussulla <maciek.kowalczyk@yandex.com>
2022-09-08 21:12:59 +0200

    write js for the modal and modal-bgc

---
+++ sliderAndModal/script.js
@@ -0,0 +1,26 @@
+const toggle = document.getElementById('toggle'); 
+const close = document.getElementById('close'); 
+const open = document.getElementById('open'); 
+const modalContainer = document.getElementById('modal'); 
+const modal = document.querySelector('.modal'); 
+
+// Toggle nav 
+toggle.addEventListener('click', ()=> {
+  document.body.classList.toggle('show-nav'); 
+})
+
+// Show modal 
+open.addEventListener('click', ()=> {
+  modalContainer.classList.add('show-modal'); 
+
+  modal.classList.add('visible'); 
+  console.log("click"); 
+})
+
+// Hide modal 
+close.addEventListener('click', ()=> {
+  modalContainer.classList.remove('show-modal'); 
+  modal.classList.remove('visible'); 
+  console.log("click"); 
+})
+
