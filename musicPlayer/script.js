5c124260a41a5045ba03080f79f706fc5c1534d9
luciussulla <maciek.kowalczyk@yandex.com>
2022-09-29 14:57:32 +0200

    Make the audio interface buttons work

--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -55,3 +55,49 @@ playBtn.addEventListener('click', ()=> {
     playSong(); 
   }
 })
+
+function prevSong() {
+  songIndex--
+  if(songIndex < 0 ) {
+    songIndex = songs.length - 1; 
+  }
+  loadSong(songs[songIndex])
+  playSong(); 
+}
+
+function nextSong() {
+  songIndex++
+  if(songIndex > songs.length - 1) {
+    songIndex = 0 
+  }
+  loadSong(songs[songIndex])
+  playSong(); 
+}
+
+// Update progress bar 
+function updateProgress(e) {
+  const {duration, currentTime } = e.srcElement; 
+  const progressPercent = (currentTime / duration) * 100; 
+  progress.style.width = `${progressPercent}%`
+}
+
+// Set prgress bar 
+function setProgress(e) {
+  const width = this.clientWidth; 
+  const clickX = e.offsetX; 
+  const duration = audio.duration; 
+  audio.currentTime = (clickX/width) * duration; 
+}
+
+// Change song 
+prevBtn.addEventListener('click', prevSong); 
+prevBtn.addEventListener('click', nextSong); 
+
+// Time / sing update 
+audio.addEventListener('timeupdate', updateProgress); 
+
+// Click on progress bar 
+progressContainer.addEventListener('click', setProgress); 
+
+// song ends 
+audio.addEventListener('ended', nextSong)
