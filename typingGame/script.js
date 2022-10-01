05687da5b86a9728bd66cd7ba56000c3267c6412
luciussulla <maciek.kowalczyk@yandex.com>
2022-10-01 11:30:42 +0200

    Calculate the time and finish game

--- typingGame/script.js
+++ typingGame/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text'); 
 const scoreEl = document.getElementById('score'); 
 const timeEl = document.getElementById('time'); 
-const endgameEl = document.getElementById('end-game'); 
+const endgameEl = document.getElementById('end-game-container'); 
 const settingsBtn = document.getElementById('settings-btn'); 
 const settings = document.getElementById('settings'); 
 const settingsForm = document.getElementById('settings-form'); 
@@ -24,6 +24,12 @@ let score = 0;
 // Init time 
 let time = 10; 
 
+// Focus on text on start 
+text.focus(); 
+
+// Start counting down 
+const timeInterval = setInterval(updateTime, 1000); 
+
 // random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -41,6 +47,29 @@ function updateScore() {
   scoreEl.innerHTML = score; 
 }
 
+// update time 
+function updateTime() {
+  time--; 
+  timeEl.innerHTML = time + 's'; 
+
+  if(time === 0) {
+    clearInterval(time); 
+    // end game 
+    gameOver(); 
+  }
+}
+
+// Game over, show end screen
+
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1> Time ran out </h1>
+    <p>  Your final score is ${score} </p>
+    <button onlick="location.reload()">Reload</button>
+  `; 
+  endgameEl.style.display = 'flex'; 
+}
+
 addWordToDOM(); 
 
 // Event listeners 
@@ -53,6 +82,5 @@ text.addEventListener('input', e => {
     updateScore(); 
     // Clear input 
     e.target.value = '';
-
   }
-})
+}); 
