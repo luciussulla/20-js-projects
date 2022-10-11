73380087e3450a771b446fe2278631af5a494611
luciussulla <maciek.kowalczyk@yandex.com>
2022-10-11 09:45:01 +0200

    show the cards dynamically with js and make the card turn' ;

---
+++ memoryCards/script.js
@@ -0,0 +1,80 @@
+const cardsContainer = document.getElementById('cards-container'); 
+const prevBtn = document.getElementById('prev'); 
+const nextBtn = document.getElementById('next'); 
+const currentEl = document.getElementById('current'); 
+const showBtn = document.getElementById('show'); 
+const hideBtn = document.getElementById('hide'); 
+const questionEl = document.getElementById('question'); 
+const answerEl = document.getElementById('answer'); 
+const addCardBtn = document.getElementById('add-card'); 
+const clearBtn = document.getElementById('clean'); 
+const addContainer = document.getElementById('add-container'); 
+
+// Keep track of current card
+let currentActiveCard = 0; 
+
+// Store DOM cards 
+const cardsEl = []; 
+
+// Store card data 
+const cardsData = [
+  {
+    question: 'What must a variable begin with?', 
+    answer: 'A letter, $ or _'
+  }, 
+  {
+    question: 'What is a variable?', 
+    answer: 'COntainer for a piece of data'
+  }, 
+  {
+    question: 'Example of Case Sensiitr variabel', 
+    answer: 'this is a variable'
+  }
+]
+
+// Create all cards 
+function createCards() {
+  cardsData.forEach((data, idx)=> createCard(data, idx)); 
+}
+
+//create a single card in DOM 
+function createCard(data, idx) {
+  const card = document.createElement('div'); 
+  card.classList.add('card'); 
+
+  if(idx===0) {
+    card.classList.add('active'); 
+  }
+
+  card.innerHTML = `
+  <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+  `; 
+
+  card.addEventListener('click', ()=> card.classList.toggle('show-answer')); 
+
+  // Add to DOM cards 
+  cardsEl.push(card); 
+  cardsContainer.appendChild(card); 
+
+  updateCurrentText()
+}
+
+// show number of cards 
+
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard +1 }/${cardsEl.length}`; 
+}
+
+createCards(); 
+
