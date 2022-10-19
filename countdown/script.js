012faac6b703944fc584d107423e5a9c5520809a
luciussulla <maciek.kowalczyk@yandex.com>
2022-10-19 06:32:37 +0200

    count down the time to new year. Do the js countdown logic

---
+++ countdown/script.js
@@ -0,0 +1,34 @@
+console.log("loaded"); 
+
+const days = document.getElementById('days'); 
+const hours = document.getElementById('hours'); 
+const minutes = document.getElementById('minutes'); 
+const seconds = document.getElementById('seconds'); 
+const countdown = document.getElementById('countdown');
+
+const currentYear = new Date().getFullYear(); 
+
+const newYearTime = new Date(`January 01 ${currentYear+1} 00:00:00`);
+
+function updateCountdown() {
+  const currentTime = new Date(); 
+  const diff = newYearTime - currentTime; 
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24; 
+  const m = Math.floor(diff / 1000 / 60) % 60; 
+  const s = Math.floor(diff / 1000) % 60; 
+
+  console.log(diff); 
+  days.innerHTML = d; 
+  hours.innerHTML =   h < 10 ? `0${h}` : h; 
+  minutes.innerHTML = m < 10 ? `0${m}` : m; 
+  seconds.innerHTML = s < 10 ? `0${s}` : s; 
+  //  hours.innerHTML =   h; 
+  //  minutes.innerHTML = m; 
+  //  seconds.innerHTML = s; 
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000); 
