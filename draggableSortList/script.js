bcc8a81543c04133f40ee1f35ddcde6dd47c91f4
luciussulla <maciek.kowalczyk@yandex.com>
2022-10-24 22:00:06 +0200

    Scramble the array based on randomized Math.random function and display all listed people

---
+++ draggableSortList/script.js
@@ -0,0 +1,45 @@
+const draggable_list = document.getElementById('draggable-list'); 
+const check = document.getElementById('check');
+
+const richestPeople = [
+  "Jeff Szmesoz", 
+  "Smill Gates", 
+  "Szmoren Szmuffet", 
+  "Bernard Arnualt", 
+  "Amacio Ortegs", 
+  "Caros Slim", 
+  "Larry Ellison", 
+  "Marz Zucekrbert"
+]
+
+// store list items
+const listItems = []; 
+
+let dragStartIndex; 
+
+createList(); 
+
+// insert list items to DOM
+
+function createList () {
+  [...richestPeople]
+  .map((a)=> ({value: a, sort: Math.random()}))
+  .sort((a,b) => a.sort-b.sort)
+  .map(obj=> obj.value)
+  .forEach((person, index) => {
+    console.log(person); 
+    const listItem = document.createElement('li'); 
+    listItem.setAttribute('data-index', index); 
+    listItem.innerHTML = `
+      <span class="numer">${index+1}</span>
+      <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-line"></i>
+      </div>
+    `; 
+
+    listItems.push(listItem); 
+
+    draggable_list.appendChild(listItem); 
+  }); 
+}
